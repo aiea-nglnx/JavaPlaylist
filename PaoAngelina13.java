ab6b0924a8768818990b4cf352e83696e4b03faa
Angelina <167708709+aiea-nglnx@users.noreply.github.com>
2025-05-20 20:59:03 -1000

    Update following files
    - NumberException
       - Delete unnecessary code
    
    - Song
       - Adjust self-validating objects
       - Generally, fix the class

---
+++ PaoAngelina13.java
@@ -0,0 +1,268 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.File;
+
+/**
+ * An interactive program Java program that models a music playlist, demonstrating the student's 
+ * understanding of objects, methods, and ArrayList. A custom exception will be 
+ * created to prevent runtime errors.
+ * 
+ * @author     Pao, Angelina
+ * @assignment ICS 111 Assignment 13
+ * @date       05/04/2025
+ * @bugs       Short description of bugs in the program, if any.
+ */
+ 
+public class PaoAngelina13 {
+   private static ArrayList<Song> playlist;
+   public static void main(String[] args) {
+   
+      // Declaring variables and initializing resources
+      Scanner reader = new Scanner(System.in);
+      String greetings = ""; // Stores the user's name
+      
+      // Prompt the user to enter their name and greet them when the program runs
+      System.out.print("Greetings, what's your name? ");
+      greetings = reader.nextLine();
+      System.out.println("\nHey " + greetings.toUpperCase() + "!" + " This program will display " +
+         "the songs in my 'Unstrung Emotions' playlist!");
+      
+      // Adding at least 5 Song objects
+      try {
+         playlist.add(new Song("Oceans & Engines", "NIKI", 5, 36));
+         playlist.add(new Song("When the Rain Stops", "eaJ", 3, 23));
+         playlist.add(new Song("Stay a Little Longer", "Rose", 4, 07));
+         playlist.add(new Song("I Know It Won't Work", "Grace Abrams", 4, 06));
+         playlist.add(new Song("Steal The Show", "Lauv", 3, 13));
+      }
+      catch (NumberException ne) {
+         System.out.println("Error adding song: " + ne.getMessage());
+      }
+      // Calling the createPlaylist method
+      createPlaylist(playlist);
+      
+      // Calling the editPlaylist method
+      playlist = editPlaylist(playlist, reader);
+      
+      // Calling the savePlaylist method
+      playlist = savePlaylist(playlist, reader);
+   }
+   
+/**
+ * A createPlaylist that prints out the ArrayList in order with Song Objects separated
+ * 
+ * @param       none
+ * @return      the ArrayList in order with the Song objects    
+ * @exception   
+ */ 
+   // createPlayList method
+   public static ArrayList<Song> createPlaylist() {
+     // Create the Arraylist
+     ArrayList<Song> playlist = new ArrayList<>();
+      try {
+      
+      }
+      catch ( ) {
+      
+      }
+      return playlist;
+   }
+   
+/**
+ * A editPlaylist that after the ArrayList of Song object that are passed
+ * allows the user to remove or add a song
+ * 
+ * @param       ArrayList<Song> playlist to hold & retrieve the Song object
+ * @param       Scanner reader to hold user input
+ * @return      playlist  
+ * @exception   NumberException to ensure number isn't negative
+ * @exception   InputMismatchException to make sure user enters the wrong type
+ */ 
+   // editPlayList method
+   public static ArrayList<Song> editPlaylist(ArrayList<Song> playlist, Scanner reader) {
+      boolean editing = true;
+     
+      while (editing) {
+         // Ask the user if they want to make changes to the playlist
+         String userResponse = "";
+      
+         System.out.print("\nWhat do you think? Should I make any changes to the playlist?" + 
+            "\n(Or if you change your mind, you can say nevermind.)" + "\nType add, remove, or nevermind: " + userResponse);
+         userResponse = reader.nextLine();
+         
+         if (userResponse.toLowerCase().equals("add")) {
+            try {
+               System.out.println("\n\tOkay, let's add a song! Please enter the song information: ");
+                  
+               System.out.print("\t\tSong Title: ");
+               String title = reader.nextLine();
+            
+               System.out.print("\t\tArtist: ");
+               String artist = reader.nextLine();
+            
+               int minutes = -1;
+               while (minutes < 0) {
+                  System.out.print("\t\tMinutes: ");
+                  try {
+                     minutes = reader.nextInt();
+                     reader.nextLine();   
+                    
+                     if (minutes < 0) {
+                        throw new NumberException("\tSorry but " + minutes + " isn't a proper or positive numerical value. Please try again.");
+                     }
+                  }      
+                  catch (java.util.InputMismatchException ime) {
+                     System.out.println("\nERROR! You must enter a valid number.");
+                     reader.nextLine();
+                  } 
+                  catch (NumberException ne) {
+                     System.out.println(ne.getMessage());
+                  }
+               }    
+               int seconds = -1;
+               while (seconds < 0) {
+                  System.out.print("\t\tSeconds: ");
+                  try {
+                     if (!reader.hasNextInt()) { // Handles non-numeric input (including spaces)
+                        System.out.println("\n\tERROR! You must enter a valid number. Please try again!");
+                        reader.nextLine(); // Clear invalid input
+                        continue;
+                     }
+                  
+                     seconds = reader.nextInt();
+                     reader.nextLine();   
+                    
+                     if (seconds < 0) {
+                        throw new NumberException("\tSorry but " + seconds + " isn't a proper or positive numerical value. Please try again.");
+                     }
+                  }      
+                  catch (java.util.InputMismatchException ime) {
+                     System.out.println("\nERROR! You must enter a valid number.");
+                     reader.nextLine();
+                  } 
+                  catch (NumberException ne) {
+                     System.out.println(ne.getMessage());
+                  }
+               }           
+            
+               System.out.print("\tWhat # song in the playlist should it be: ");
+                  
+               playlist.add(reader.nextInt() - 1, new Song(title, artist, minutes, seconds));
+               reader.nextLine();
+               
+               System.out.println("\nNow adding \'" + playlist.get(playlist.size() - 1).getTitle() + "\' as #" + playlist.size() + " on the playlist...");
+               System.out.println("\nHere's the new playlist: ");
+               for (int i = 0; i < playlist.size(); i++) {
+                  System.out.println((i + 1) + ". " + playlist.get(i)); // Print all songs correctly
+               }
+            }
+            catch (java.util.InputMismatchException ime) {
+               System.out.println("ERROR! You must enter a valid number.");
+            }
+            catch (NumberException ne) {
+               System.out.println("ERROR! " + ne.getMessage());
+            }  
+         }
+         else if (userResponse.toLowerCase().equals("nevermind")) {
+            System.out.println("\nNo changes made. Returning to the playlist view.");
+            editing = false;
+         }
+         else if (userResponse.toLowerCase().equals("remove")) {
+             // Removes the song
+            System.out.println("\nOkay!, Just in case you forgot, here is the current playlist: ");
+               
+            System.out.println("\n***Unstrung Emotions Playlist***");
+            for (int i = 0; i < playlist.size(); i++) {
+               System.out.println((i + 1) + ". " + playlist.get(i));
+            }
+            System.out.print("Which # song would you like to remove: ");             
+            if (reader.hasNextInt()) {
+               Song removedSong = playlist.remove(reader.nextInt() - 1);
+               reader.nextLine();
+               System.out.println("\nNow removing \"" + removedSong.getTitle() + "\" by " + removedSong.getArtist() + 
+                  " (" + removedSong.getMinutes() + ":" + 
+                  String.format("%02d", removedSong.getSeconds()) + ")...");
+               
+               System.out.println("\nHere's the new playlist: ");
+               for (int i = 0; i < playlist.size(); i++) {
+                  System.out.println((i + 1) + ". " + playlist.get(i)); // Print all songs correctly
+               }
+            
+            } 
+            else {
+               System.out.println("ERROR! You need to type a number.");
+               reader.nextLine();
+            }
+         }
+         else {
+            System.out.println("\nInvalid input! Please type add, remove, or nevermind.");
+         }
+      
+      }
+      return playlist;  
+   }
+     
+/**
+ * A savePlaylist that serves as an indicator for whether the user would like
+ * like to append to or overwrite a file.
+ * 
+ * @param       ArrayList<Song> playlist to hold & retrieve the Song object
+ * @param       Scanner reader to hold user input
+ * @return      playlist    
+ * @exception   none
+ */ 
+   // savePlayList method
+   
+   public static ArrayList<Song> savePlaylist(ArrayList<Song> playlist, Scanner reader) {
+    System.out.print("\nOnce you close this program, you're gonna lose all the playlist data."
+        + "\nWould you like to save it to a file, so you can refer to it later?"
+        + "\nType yes or no: ");
+    
+    String response = reader.nextLine().toLowerCase(); 
+
+    if (response.equals("yes")) { // Using your preferred approach
+        File outFH = new File("playlist.txt");
+
+        System.out.print("\nWould you like to append to or overwrite this file?"
+            + "\nType append or overwrite: ");
+        
+        response = reader.nextLine().toLowerCase(); // Reuse response
+
+        boolean append = response.equals("append"); 
+
+        // Format the date when the playlist is saved
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentDate = LocalDateTime.now().format(formatter);
+
+        try (FileWriter outFW = new FileWriter(outFH, append)) {
+            outFW.write("Playlist saved on: " + currentDate + "\n");
+            outFW.write("------------------------------------\n");
+
+            if (playlist.isEmpty()) {
+                outFW.write("No songs in the playlist.\n");
+            } 
+            else {
+                for (int i = 0; i < playlist.size(); i++) {
+                    Song song = playlist.get(i);
+                    outFW.write((i + 1) + ". " + song.getTitle() + " - "
+                                + song.getArtist() + " (" + song.getMinutes() + ":"
+                                + String.format("%02d", song.getSeconds()) + ")\n");
+                }
+            }
+
+            System.out.println("\n   Playlist is now saved to: " + outFH.getAbsolutePath());
+        } 
+        catch (IOException ioe) {
+            System.out.println("An error occurred while saving the playlist. Please restart the program!");
+        }
+    } 
+    else { // Keeping your preferred structure
+        System.out.println("That's okay.");
+    }
+    return playlist;
+}
+}
---
+++ Song.java
@@ -0,0 +1,112 @@
+/**
+ * An interactive program Java program that models a music playlist
+ * demonstrating the student's understanding of objects and ArrayList.
+ * 
+ * @author     Pao, Angelina
+ * @assignment ICS 111 Assignment 11 - Song Object
+ * @date       04/20/2025
+ * @bugs       Short description of bugs in the program, if any.
+ */
+ 
+ public class Song {
+   // Instance variables that represent Object 1's characteristics   
+      private String title = ""; // Stores the title of the song
+      private String artist = ""; // Stores the artist of the song
+      private int minutes = 0; // Stores the minutes of the song
+      private int seconds = 0; // Stores the seconds of the song
+   
+/**
+ * Constructs a Song object with specified attributes.
+ *
+ * @param title     The title of the song (must be at least 2 characters long).
+ * @param artist    The name of the song's artist (must be at least 2 characters long).
+ * @param minutes   The minutes of the song (must be a positive integer).
+ * @param seconds   The seconds of the song (must be positive and can't be over 60).
+ * @throws IllegalArgumentException   If the title or artist is too short, or the type is invalid.
+ * @throws NumberException            If minutes or seconds is negative.
+ */
+   
+   // Constructor that allows you to create the Song object
+   public Song(String title, String artist, int minutes, int seconds) throws NumberException {
+      settitle(title);
+      setartist(artist);
+      setminutes(minutes);
+      setseconds(seconds);
+      
+      this.title = title;
+      this.artist = artist;
+      this.minutes = minutes;
+      this.seconds = seconds;
+
+   }
+   
+   /**
+ * Returns a string representation of the Song object.
+ *
+ * @return A formatted string containing the song's title, artist, minutes & seconds duration.
+ *         Example format:
+ *         Title: [title]
+ *         Artist: [artist]
+ *         Minutes: [minutes]
+ *         Seconds: [seconds]
+ */
+
+   // toString method
+   public String toString() {
+      String output = "";
+      output += "\"" + this.title + "\""; // Adds song title to output
+      output += " by " + this.artist + " "; // Adds artist of the song to output
+      output += "\n" + this.minutes + " minutes "; // Adds the minutes of the song to output
+      output += this.seconds + " seconds\n"; // Adds the seconds of the song to output
+      
+      return output;
+   }
+   // Accessor Methods to Song class that retrieves the songs's attributes
+   public String getTitle() {
+      return this.title;
+   }
+   public String getArtist() {
+      return this.artist;
+   }
+   public int getMinutes() {
+      return this.minutes;
+   }
+   public int getSeconds() {
+      return this.seconds;
+   }
+   
+   // Adding Mutator methods to Song class that allows modification
+   public void settitle(String newTitle) throws IllegalArgumentException {
+      if (newTitle.length() >= 2) { 
+      this.title = newTitle;
+      }
+      else {
+         throw new IllegalArgumentException("ERROR! Song title must be at least 2 characters long!!");
+      }
+   }
+   public void setartist(String newArtist) throws IllegalArgumentException {
+      if (newArtist.length() >= 2) {
+         this.artist = newArtist;
+      }
+      else {
+         throw new IllegalArgumentException("ERROR! Artist must be at least 2 characters long!");
+      }
+   }
+   public void setminutes(int newMinutes) throws NumberException {
+      if (newMinutes > 0) {
+         this.minutes = newMinutes;
+      }
+      else {
+         throw new NumberException("ERROR! Sorry but " + newMinutes + " isn't a proper or positive numerical value.");
+      }
+      
+   }
+   public void setseconds(int newSeconds) throws NumberException {
+      if (newSeconds > 0 || seconds < 59) {
+         this.seconds = newSeconds;
+      }
+      else {
+         throw new NumberException("Sorry but " + newSeconds + " must be above the value 0 and can't be over 59.");
+      }
+   }
+}
